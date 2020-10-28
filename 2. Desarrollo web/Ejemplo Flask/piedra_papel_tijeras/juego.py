05617e2a5600c18ea4283e6504981fd771e3395d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-10-28 16:22:22 +0000

    Añade las reglas completas del juego

--- 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
+++ 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template
 from datetime import date
+from random import choice
 
 app = Flask(__name__)
+
+reglas = {
+    'piedra': {
+        'piedra':  {'resultado': 0,  'mensaje': 'Empate'},
+        'papel':   {'resultado': -1, 'mensaje': 'Papel envuelve piedra'},
+        'tijeras': {'resultado': 1,  'mensaje': 'Piedra aplasta tijeras'}
+    },
+    'papel': {
+        'piedra':  {'resultado': 1,  'mensaje': 'Papel envuelve piedra'},
+        'papel':   {'resultado': 0,  'mensaje': 'Empate'},
+        'tijeras': {'resultado': -1, 'mensaje': 'Tijeras cortan papel'}
+    },
+    'tijeras': {
+        'piedra':  {'resultado': -1, 'mensaje': 'Piedra aplasta tijeras'},
+        'papel':   {'resultado': 1,  'mensaje': 'Tijeras cortan papel'},
+        'tijeras': {'resultado': 0,  'mensaje': 'Empate'},
+    }
+}
+
+objetos = list(reglas.keys())
 
 @app.route('/')
 def inicio():
@@ -12,9 +33,9 @@ def inicio():
 
 @app.route('/tiro/<tiro_jugador>')
 def tiro(tiro_jugador):
-    tiro_compu = 'piedra'
-    resultado = 1
-    mensaje = f'{tiro_jugador} vence a {tiro_compu}'
+    tiro_compu = choice(objetos)
+    resultado = reglas[tiro_jugador][tiro_compu]['resultado']
+    mensaje = reglas[tiro_jugador][tiro_compu]['mensaje']
     return render_template(
         'despliega_resultado.html',
         tiro_jugador=tiro_jugador,
