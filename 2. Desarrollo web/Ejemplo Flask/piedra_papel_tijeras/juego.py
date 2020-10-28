aae78806b62333f75b4240a4130070a73bf61d62
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-10-28 15:25:57 +0000

    Añade código dinámico para la fecha de hoy

--- 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
+++ 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('selecciona_tiro.html')
+    fecha = date.today()
+    return render_template(
+        'selecciona_tiro.html',
+        fecha=fecha)
