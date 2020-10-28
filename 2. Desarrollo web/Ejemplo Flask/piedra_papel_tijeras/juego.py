2ecf5bd7432e65f299fa1704db142a26a35b6e60
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-10-28 15:13:53 +0000

    Añade el archivo selecciona_tiro.html

--- 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
+++ 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return '¡Hola, mundo!'
+    return render_template('selecciona_tiro.html')
