e48e51c107db8d707d17313d941823737652e1e1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-10-28 15:54:08 +0000

    Añade el archivo despliega_resultado.html

--- 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
+++ 2. Desarrollo web/Ejemplo Flask/piedra_papel_tijeras/juego.py
@@ -9,3 +9,15 @@ def inicio():
     return render_template(
         'selecciona_tiro.html',
         fecha=fecha)
+
+@app.route('/tiro/<tiro_jugador>')
+def tiro(tiro_jugador):
+    tiro_compu = 'piedra'
+    resultado = 1
+    mensaje = f'{tiro_jugador} vence a {tiro_compu}'
+    return render_template(
+        'despliega_resultado.html',
+        tiro_jugador=tiro_jugador,
+        tiro_compu=tiro_compu,
+        resultado=resultado,
+        mensaje=mensaje)
